51b5fe9cb9e1e1f538a3da45a1b0511acc629931
theitendotnet <80585677+theitendotnet@users.noreply.github.com>
2023-01-23 13:02:04 +0100

    electron ui added with a blazor server that calls the pwXt-cli

---
+++ src/pwXt-ui/Program.cs
@@ -0,0 +1,43 @@
+using ElectronNET.API;
+using pwXt_ui.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddElectron();
+builder.WebHost.UseElectron(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<ToastService>();
+builder.Services.AddSingleton(
+    sp => new PwStore(builder.Configuration.GetSection("AppSettings:CliPath").Value,
+    sp.GetRequiredService<ToastService>())
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(e =>
+{
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+});
+
+var t = Task.Run(() => app.Run());
+var t1 = Task.Run(async () => await Electron.WindowManager.CreateBrowserViewAsync());
+var t2 = Task.Run(async () => await Electron.WindowManager.CreateWindowAsync());
+await Task.WhenAll(t, t1, t2);
---
+++ src/pwXt-ui/Shared/Toaster.razor.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Components;
+using pwXt_ui.Data;
+namespace pwXt_ui.Shared;
+
+public class ToasterBase : ComponentBase, IDisposable
+{
+    [Inject] 
+    private ToastService ToastService { get; set; } = null!;
+
+    protected string Heading { get; set; } = null!;
+    protected string Message { get; set; } = null!;
+    protected bool IsVisible { get; set; }
+    protected string BackgroundCssClass { get; set; } = null!;
+    protected string IconCssClass { get; set; } = null!;
+
+    protected override void OnInitialized()
+    {
+        ToastService.OnShow += ShowToast;
+        ToastService.OnHide += HideToast;
+    }
+
+    private void ShowToast(string message, ToastService.ToastLevel level)
+    {
+        BuildToastSettings(level, message);
+        IsVisible = true;
+        InvokeAsync(StateHasChanged);
+    }
+
+    private void HideToast()
+    {
+        IsVisible = false;
+        InvokeAsync(StateHasChanged);
+    }
+
+    private void BuildToastSettings(ToastService.ToastLevel level, string message)
+    {
+        switch (level)
+        {
+            case ToastService.ToastLevel.Info:
+                BackgroundCssClass = "bg-info";
+                IconCssClass = "info";
+                Heading = "Info";
+                break;
+            case ToastService.ToastLevel.Success:
+                BackgroundCssClass = "bg-success";
+                IconCssClass = "check";
+                Heading = "Success";
+                break;
+            case ToastService.ToastLevel.Warning:
+                BackgroundCssClass = "bg-warning";
+                IconCssClass = "exclamation";
+                Heading = "Warning";
+                break;
+            case ToastService.ToastLevel.Error:
+                BackgroundCssClass = "bg-danger";
+                IconCssClass = "times";
+                Heading = "Error";
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(level), level, null);
+        }
+
+        Message = message;
+    }
+
+    public void Dispose()
+    {
+        ToastService.OnShow -= ShowToast;
+        ToastService.OnHide -= HideToast;
+    }
+}
